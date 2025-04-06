e4197bebacf4b150e5ee56849f1307cc150dc0e2
Seryzedd <aurelienbard@hotmail.fr>
2025-04-06 16:35:17 +0200

    Adding debug toolbar

--- CV-Generator/src/Controller/HomepageController.php
+++ CV-Generator/src/Controller/HomepageController.php
@@ -5,12 +5,18 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpKernel\Profiler\Profiler;
 
 final class HomepageController extends AbstractController
 {
     #[Route('/', name: 'app_homepage')]
-    public function index(): Response
+    public function index(?Profiler $profiler): Response
     {
+        if (null !== $profiler) {
+            // if it exists, disable the profiler for this particular controller action
+            $profiler->disable();
+        }
+        
         return $this->render('homepage/index.html.twig', [
             'controller_name' => 'HomepageController',
         ]);
